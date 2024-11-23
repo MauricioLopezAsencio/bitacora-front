fea0ac568e19c12518b4336d63165758d5c844e4
mauguetta <mauriciolopezasencio@gmail.com>
2024-11-22 22:12:30 -0600

    cambio de instancia de backend

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -1,4 +1,4 @@
 export const Constants ={
     //baseUrl: 'http://localhost:3000/api/v1/'
-    baseUrl: 'https://bitacora-back-end.onrender.com/api/v1/'
+    baseUrl: 'https://young-mosquito-mlopez-65d9ed66.koyeb.app/api/v1/'
 }
